125acf53fc8d93a166c4b9122a39aaa5ab2997a2
Owen <owshepherd@gmail.com>
2019-03-05 21:31:16 +1100

    Reworking Book class to two individual classes

--- terminal-app.rb
+++ terminal-app.rb
@@ -1,35 +1,61 @@
-class Book
-    attr_accessor :title, :author
-    def initialize(title, author)
-        @title = title
+class Author
+    attr_accessor :author, :books
+    def initialize (author)
         @author = author
+        @books = []
     end
 
-    def book_search (search_term)
-        if @title.include?(search_term) || @author.include?(search_term)
-            return "#{@title} by #{@author}"
+    def add_book (book) # Asterisks is used to add multiple parameters
+        @books << book
+    end
+
+    def display
+        puts "#{@author} #{@books}"
+    end
+
+    def search (search_term)
+        if @author.include?(search_term) || @books.include?(search_term)
+           p "#{@author}, #{@books}"
         end
     end
 end
 
-book_library = [
-book1 = Book.new("Harry Potter", "JK Rowling"),
-book2 = Book.new("The Green Mile", "Stephen King"),
-book3 = Book.new("Dracula", "Bram Stoker")
-]
-
-# book3.book_search("Dracula")
-
-search_term = gets.chomp
-book_library.select { |array| array.book_search(search_term) == true }
-
-def recommend (title, author, keyword)
+class Book
+    def initialize (book) #maybe change to title
+        @book = book
+    end
 end
 
-user = {
-    user_name: name
-    books_read_by_user: []
-    authors_read_by_user: []
-    user_likes_these_genres: []
-}
+author1 = Author.new ("Stephen King")
+book1 = Book.new ("Green Mile")
+book2 = Book.new ("Christine")
+book3 = Book.new ("The Shining")
+author1.add_book(book1)
+author1.add_book(book2)
+author1.add_book(book3)
+
+author2 = Author.new ("JRR Tolkein")
+book4 = Book.new ("The Hobbit")
+author2.add_book(book4)
+
+author3 = Author.new ("Some Person")
+book5 = Book.new ("The Juror")
+author3.add_book(book5)
+
+book_array = [author1, author2, author3]
+
+# book_array = [book1, book2, book3, book4]
+
+search_term = gets.chomp
+book_array.select { |array| array.search(search_term) == true }
+
+# def recommend (title, author, keyword)
+# end
+
+# user = {
+#     user_name: name
+#     books_read_by_user: []
+#     authors_read_by_user: []
+#     user_likes_these_genres: []
+# }
 
